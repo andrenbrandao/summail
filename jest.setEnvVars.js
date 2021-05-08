5cc68ef74ec35c582ee17e98048703d8bbc62f9c
André Brandão <andrenbrandao@gmail.com>
2021-05-08 12:13:58 -0300

    test: send message to sqs

--- jest.setEnvVars.js
+++ jest.setEnvVars.js
@@ -3,3 +3,5 @@ process.env.OAUTH_CLIENT_ID = 'client-id';
 process.env.OAUTH_CLIENT_SECRET = 'client-secret';
 process.env.OAUTH_REDIRECT_URI = 'http://localhost:4000/dev/oauth-callback';
 process.env.OAUTH_URI = 'https://accounts.google.com/o/oauth2/auth';
+process.env.GMAIL_NOTIFICATION_QUEUE_URL =
+  'https://sqs.us-east-1.amazonaws.com/queue';
--- src/functions/gmail-push/handler.spec.ts
+++ src/functions/gmail-push/handler.spec.ts
@@ -1,12 +1,17 @@
 import type { Context, Callback } from 'aws-lambda';
+import { sendMessage } from '@services/sqs';
 import { main as handler } from './handler';
 
 import mockEvent from './mock.json';
 
 jest.mock('@services/sqs');
 
+const mockedSendMessage = sendMessage as jest.MockedFunction<
+  typeof sendMessage
+>;
+
 it('should log base64-encoded data', async () => {
-  const event = mockEvent;
+  const event = { ...mockEvent };
   const context = {} as Context;
   const callback = null as Callback;
 
@@ -18,5 +23,22 @@ it('should log base64-encoded data', async () => {
       emailAddress: 'user@example.com',
       historyId: '1234567890',
     }),
+  });
+});
+
+it('should call sqs sendMessage with notification data', async () => {
+  const event = { ...mockEvent };
+  const context = {} as Context;
+  const callback = null as Callback;
+
+  await handler(event, context, callback);
+
+  expect(mockedSendMessage).toHaveBeenCalledWith({
+    MessageBody: JSON.stringify({
+      emailAddress: 'user@example.com',
+      historyId: '1234567890',
+    }),
+    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/queue',
+    MessageGroupId: 'user@example.com',
   });
 });
