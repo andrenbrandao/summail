ddb516f447898f4ff6f7903aba807a3555418472
André Brandão <andrenbrandao@gmail.com>
2021-08-08 19:38:20 -0300

    fix: ignore historyIds that gmail considers as not found

--- src/functions/save-email/getUserAndSaveMessages.ts
+++ src/functions/save-email/getUserAndSaveMessages.ts
@@ -6,16 +6,20 @@ import {
   refreshAccessToken,
   getHistoryMessagesByHistoryId,
 } from '@services/google';
+import { logger } from '@shared/logger';
 import { saveUserLastMessages } from './saveUserLastMessages';
 
 export async function getUserAndSaveMessages(
   emailAddress: string,
   historyId: string,
 ): Promise<void> {
-  const { refreshToken, lastHistoryId } = await getUser(emailAddress);
+  const { email, refreshToken, lastHistoryId } = await getUser(emailAddress);
 
   const accessToken = await refreshAccessToken(refreshToken);
 
+  logger.info(
+    `Querying history for ${email} with startHistoryId: ${lastHistoryId}`,
+  );
   const historyMessages = await getHistoryMessagesByHistoryId(
     accessToken,
     lastHistoryId,
--- src/services/google/getHistoryMessagesByHistoryId.ts
+++ src/services/google/getHistoryMessagesByHistoryId.ts
@@ -1,4 +1,5 @@
 import { google } from 'googleapis';
+import { logger } from '@shared/logger';
 
 import { HistoryMessage } from '@shared/interfaces';
 
@@ -12,19 +13,30 @@ const getHistoryMessagesByHistoryId = async (
 
   const gmail = google.gmail({ version: 'v1' });
 
-  const response = await gmail.users.history.list({
-    startHistoryId: historyId,
-    historyTypes: ['MESSAGE_ADDED'],
-    userId: 'me',
-    access_token: accessToken,
-  });
+  try {
+    const response = await gmail.users.history.list({
+      startHistoryId: historyId,
+      historyTypes: ['MESSAGE_ADDED'],
+      userId: 'me',
+      access_token: accessToken,
+    });
 
-  const messagesAdded = response.data.history.flatMap(
-    (history) => history.messagesAdded,
-  );
-  const messages = messagesAdded.map((messageAdded) => messageAdded.message);
+    const messagesAdded = response.data.history.flatMap(
+      (history) => history.messagesAdded,
+    );
+    const messages = messagesAdded.map((messageAdded) => messageAdded.message);
 
-  return messages;
+    return messages;
+  } catch (error) {
+    if (error.response?.status !== 404) {
+      logger.error(error);
+      throw error;
+    }
+    logger.warn(
+      `Gmail returned 404 for historyId ${historyId}. It will be ignored to enable future calls.`,
+    );
+    return [];
+  }
 };
 
 export default getHistoryMessagesByHistoryId;
