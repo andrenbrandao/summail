b5ec2599a2269c212050545fbeeab8d082388d10
André Brandão <andrenbrandao@gmail.com>
2021-09-12 19:31:04 -0300

    chore: add simple send email to find bug

--- src/services/ses/sendEmail.ts
+++ src/services/ses/sendEmail.ts
@@ -1,3 +1,4 @@
+import { logger } from '@shared/logger';
 import { SESV2 } from 'aws-sdk';
 
 interface ISendEmailDTO {
@@ -10,13 +11,39 @@ const sendEmail = async ({
   emailDigest,
 }: ISendEmailDTO): Promise<void> => {
   const client = new SESV2({ region: 'us-east-1' });
-  await client
+
+  const responseRaw = await client
     .sendEmail({
       FromEmailAddress: 'newsletter@andrebrandao.me',
       Destination: { ToAddresses: [userEmail] },
       Content: { Raw: { Data: emailDigest } },
     })
     .promise();
+
+  logger.info(responseRaw);
+
+  // Simple email sender
+  // TODO: Remove when RAW is fixed
+  const response = await client
+    .sendEmail({
+      FromEmailAddress: 'newsletter@andrebrandao.me',
+      Destination: { ToAddresses: [userEmail] },
+      Content: {
+        Simple: {
+          Body: {
+            Text: {
+              Data: 'Simple email.',
+            },
+          },
+          Subject: {
+            Data: 'Newsletter',
+          },
+        },
+      },
+    })
+    .promise();
+
+  logger.info(response);
 };
 
 export default sendEmail;
