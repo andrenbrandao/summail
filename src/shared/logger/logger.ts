f9a798cefe695696ca26d90165da40bc2e123d8c
André Brandão <andrenbrandao@gmail.com>
2023-09-24 21:23:23 -0300

    fix: logger format errors

--- src/shared/logger/logger.ts
+++ src/shared/logger/logger.ts
@@ -2,7 +2,7 @@ import winston, { createLogger, format } from 'winston';
 
 const logger = createLogger({
   level: 'debug',
-  format: format.json(),
+  format: format.combine(format.errors({ stack: true }), format.json()),
   transports: [
     new winston.transports.Console({
       silent: process.env.NODE_ENV === 'test',
